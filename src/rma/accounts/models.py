c6d1e8060b46fabae74005e9430a896d0d29c115
annashamray <anna.shamray@maykinmedia.nl>
2020-06-08 12:17:34 +0200

    make email field unique

--- src/rma/accounts/models.py
+++ src/rma/accounts/models.py
@@ -20,7 +20,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     )
     first_name = models.CharField(_("first name"), max_length=255, blank=True)
     last_name = models.CharField(_("last name"), max_length=255, blank=True)
-    email = models.EmailField(_("email address"), blank=True)
+    email = models.EmailField(_("email address"), unique=True)
     is_staff = models.BooleanField(
         _("staff status"),
         default=False,
