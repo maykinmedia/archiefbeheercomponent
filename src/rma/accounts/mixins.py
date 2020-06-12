a850f803b50935c7f84eeffb868ca5da59e9a239
annashamray <anna.shamray@maykinmedia.nl>
2020-06-12 12:54:15 +0200

    process PR feedback

--- src/rma/accounts/mixins.py
+++ src/rma/accounts/mixins.py
@@ -1,4 +1,8 @@
+import logging
+
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+logger = logging.getLogger(__name__)
 
 
 class RoleRequiredMixin(LoginRequiredMixin, UserPassesTestMixin):
@@ -6,6 +10,7 @@ class RoleRequiredMixin(LoginRequiredMixin, UserPassesTestMixin):
 
     def test_func(self):
         if not self.role_permission:
+            logger.debug("no 'role_permission' is set for %r", self)
             return False
 
         role = self.request.user.role
