ef9006f0d795e4ec336b080e137433b8b13566c4
annashamray <anna.shamray@maykinmedia.nl>
2020-06-24 12:42:02 +0200

    test reviewer landing page

--- src/rma/destruction/tests/test_landing_page.py
+++ src/rma/destruction/tests/test_landing_page.py
@@ -8,6 +8,9 @@ from django_webtest import WebTest
 from rma.accounts.tests.factories import UserFactory
 from rma.notifications.tests.factories import NotificationFactory
 
+from ..constants import ReviewerDisplay
+from .factories import DestructionListFactory, DestructionListReviewFactory
+
 
 class RecordManagerTests(WebTest):
     """
@@ -61,3 +64,84 @@ class RecordManagerTests(WebTest):
         msg = "notification 0"
         with self.subTest(message=msg):
             self.assertNotContains(response, msg)
+
+
+class ReviewerTests(WebTest):
+    """
+    Test the role that can review destruction list processes.
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserFactory.create(role__can_review_destruction=True)
+
+        cls.list_to_review = DestructionListFactory.create(
+            assignee=cls.user, name="list to review"
+        )
+        cls.list_reviewed = DestructionListFactory.create(name="list reviewed")
+        DestructionListReviewFactory.create(
+            destruction_list=cls.list_reviewed, author=cls.user
+        )
+        cls.list_extra = DestructionListFactory.create(
+            author=cls.user, name="list extra"
+        )
+
+    def setUp(self) -> None:
+        super().setUp()
+
+        self.app.set_user(self.user)
+        self.url = reverse("destruction:reviewer-list")
+
+    def test_redirect(self):
+        response = self.app.get(reverse("entry")).follow()
+
+        self.assertEqual(response.request.path, self.url)
+
+    def test_filter_to_review(self):
+        response = self.app.get(self.url, {"reviewed": ReviewerDisplay.to_review})
+
+        self.assertEqual(response.status_code, 200)
+
+        destruction_lists = response.html.find_all(class_="destruction-list-preview")
+        self.assertEqual(len(destruction_lists), 1)
+
+        destruction_list = destruction_lists[0]
+        self.assertTrue(self.list_to_review.name in destruction_list.get_text())
+
+        for destr_list in [self.list_reviewed, self.list_extra]:
+            self.assertFalse(destr_list.name in destruction_list.get_text())
+
+    def test_filter_reviewed(self):
+        response = self.app.get(self.url, {"reviewed": ReviewerDisplay.reviewed})
+
+        self.assertEqual(response.status_code, 200)
+
+        destruction_lists = response.html.find_all(class_="destruction-list-preview")
+        self.assertEqual(len(destruction_lists), 1)
+
+        destruction_list = destruction_lists[0]
+        self.assertTrue(self.list_reviewed.name in destruction_list.get_text())
+
+        for destr_list in [self.list_to_review, self.list_extra]:
+            self.assertFalse(destr_list.name in destruction_list.get_text())
+
+    def test_filter_all(self):
+        response = self.app.get(self.url, {"reviewed": ReviewerDisplay.all})
+
+        self.assertEqual(response.status_code, 200)
+
+        destruction_lists = response.html.find_all(class_="destruction-list-preview")
+        self.assertEqual(len(destruction_lists), 2)
+
+        for destr_list in [self.list_to_review, self.list_reviewed]:
+            self.assertContains(response, destr_list.name)
+
+        self.assertNotContains(response, self.list_extra.name)
+
+    def test_filter_default(self):
+        response_default = self.app.get(self.url)
+        response_to_review = self.app.get(
+            self.url, {"reviewed": ReviewerDisplay.to_review}
+        )
+
+        self.assertEqual(response_default.body, response_to_review.body)
