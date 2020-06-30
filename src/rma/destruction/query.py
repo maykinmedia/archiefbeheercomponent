69c774f5e5729eee20e89d4ebb9fdf1c74c4e2cb
Sergei Maertens <sergei@maykinmedia.nl>
2020-06-30 10:19:42 +0200

    :art: Simplify reviewer query

--- src/rma/destruction/query.py
+++ src/rma/destruction/query.py
@@ -5,9 +5,4 @@ from rma.accounts.models import User
 
 class DestructionListQuerySet(models.QuerySet):
     def reviewed_by(self, user: User):
-        from .models import DestructionListReview
-
-        reviewed = DestructionListReview.objects.filter(author=user).values(
-            "destruction_list_id"
-        )
-        return self.filter(id__in=models.Subquery(reviewed))
+        return self.filter(reviews__author=user)
