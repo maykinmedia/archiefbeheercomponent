c5d8acb0c8f720ed33ae80c75c134e021ab58ae6
silvia <silvia@maykinmedia.nl>
2021-01-21 17:38:54 +0100

    :construction: [#108] Generate admin-index fixture

---
+++ bin/generate_admin_index_fixture.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Dump the current (local database) admin layout to a JSON fixture. This
+# overwrites the existing one.
+#
+# You can load this fixture with:
+# $ src/manage.py loaddata src/rma/fixtures/admin_index.json
+#
+# Run this script from the root of the repository
+
+src/manage.py dumpdata --indent=4 --natural-foreign --natural-primary admin_index.AppGroup admin_index.AppLink > src/rma/fixtures/admin_index.json
