48aa2617a0c4851ed1b291d67b7eca8b13233656
silvia <silvia@maykinmedia.nl>
2021-02-04 10:43:03 +0100

    :white_check_mark: [#125] Demo view tests

--- src/archiefvernietigingscomponent/demo/tests/test_demo.py
+++ src/archiefvernietigingscomponent/demo/tests/test_demo.py
@@ -11,6 +11,8 @@ class StartPageTests(TestCase):
 
     @classmethod
     def setUpTestData(cls):
+        super().setUpTestData()
+
         cls.user = UserFactory.create(role__can_start_destruction=True)
 
     def test_start_page_if_not_logged_in(self):
@@ -35,3 +37,60 @@ class StartPageTests(TestCase):
         self.assertEqual(200, response.status_code)
         self.assertNotIn(b"demo mode", response.content)
         self.assertNotIn(b"Tutorial", response.content)
+
+
+@override_settings(AVC_DEMO_MODE=True)
+class DemoViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+
+        cls.functioneel_beheer = UserFactory.create(
+            username="demo-functioneel-beheer",
+            role__can_start_destruction=True,
+            role__can_review_destruction=True,
+            role__can_view_case_details=True,
+        )
+        cls.process_eigenaar = UserFactory.create(
+            username="demo-process-eigenaar",
+            role__can_start_destruction=False,
+            role__can_review_destruction=True,
+            role__can_view_case_details=True,
+        )
+        cls.user = UserFactory.create(username="hello")
+
+    @override_settings(AVC_DEMO_MODE=False)
+    def test_no_demo_mode(self):
+        response = self.client.get(
+            reverse("demo-login", args=[self.functioneel_beheer.pk])
+        )
+
+        self.assertEqual(403, response.status_code)
+
+    def test_demo_mode_functioneel_beheer(self):
+        response = self.client.get(
+            reverse("demo-login", args=[self.functioneel_beheer.pk]), follow=True
+        )
+
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(
+            reverse("destruction:record-manager-list"), response.request["PATH_INFO"]
+        )
+
+    def test_demo_mode_process_eigenaar(self):
+        response = self.client.get(
+            reverse("demo-login", args=[self.process_eigenaar.pk]), follow=True
+        )
+
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(
+            reverse("destruction:reviewer-list"), response.request["PATH_INFO"]
+        )
+
+    def test_demo_mode_normal_user(self):
+        response = self.client.get(
+            reverse("demo-login", args=[self.user.pk]), follow=True
+        )
+
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(response.request["PATH_INFO"], reverse("entry"))
