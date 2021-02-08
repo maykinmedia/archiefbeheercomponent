f1164f03700f1a48668403af9742cbb637b924af
silvia <silvia@maykinmedia.nl>
2021-02-08 16:06:20 +0100

    :sparkles: [#62] Email to archivaris

--- src/archiefvernietigingscomponent/destruction/tasks.py
+++ src/archiefvernietigingscomponent/destruction/tasks.py
@@ -2,6 +2,7 @@ import logging
 import traceback
 
 from django.conf import settings
+from django.core.mail import EmailMessage, send_mail
 from django.utils.translation import gettext_lazy as _
 
 from celery import chain
@@ -10,10 +11,12 @@ from zds_client.client import ClientError
 
 from archiefvernietigingscomponent.notifications.models import Notification
 
+from ..accounts.models import User
 from ..celery import app
 from .constants import ListItemStatus, ListStatus
 from .models import DestructionList, DestructionListItem
 from .service import fetch_zaak, remove_zaak, update_zaak
+from .utils import create_destruction_report
 
 logger = logging.getLogger(__name__)
 
@@ -95,6 +98,14 @@ def process_list_item(list_item_id):
             template="destruction/logs/item_destruction_succeeded.txt",
             extra_data={"zaak": zaak["identificatie"]},
         )
+        list_item.extra_zaak_data = {
+            "identificatie": zaak["identificatie"],
+            "omschrijving": zaak.get("omschrijving") or "",
+            "toelichting": zaak.get("toelichting") or "",
+            "startdatum": zaak["startdatum"],
+            "einddatum": zaak.get("einddatum") or "",
+        }
+        list_item.save()
 
     list_item.save()
     return list_item.status
@@ -114,6 +125,22 @@ def complete_and_notify(list_id):
         user=destruction_list.author,
         message=_("Processing of the list is complete."),
     )
+
+    # Send email to archivaris role
+    if destruction_list.items.filter(status=ListItemStatus.destroyed).exists():
+        report = create_destruction_report(destruction_list)
+        email = EmailMessage(
+            subject=_("Verklaring van vernietiging"),
+            body=report,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            to=[
+                user.email
+                for user in User.objects.filter(role__can_review_destruction=True)
+            ],
+        )
+        email.content_subtype = "html"
+        email.send()
+
     return notification.id
 
 
