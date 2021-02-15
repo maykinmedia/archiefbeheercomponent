4932bfb9776f128fe1032d8124b06e25cb099aaa
silvia <silvia@maykinmedia.nl>
2021-02-15 14:29:28 +0100

    :lipstick: Updated start-tutorial-button

--- src/archiefvernietigingscomponent/demo/templatetags/demo_tutorial.py
+++ src/archiefvernietigingscomponent/demo/templatetags/demo_tutorial.py
@@ -1,4 +1,5 @@
 from django import template
+from django.urls import reverse
 
 from archiefvernietigingscomponent.accounts.models import User
 
@@ -9,3 +10,10 @@ register = template.Library()
 def demo_tutorial(current_user: User) -> dict:
     demo_users = User.objects.filter(username__startswith="demo").order_by("pk")
     return {"demo_users": demo_users, "current_user": current_user}
+
+
+@register.simple_tag
+def demo_record_manager_login_url() -> int:
+    first_demo_user = User.objects.get(username="demo-record-manager")
+    url = reverse("demo-login", args=[first_demo_user.pk])
+    return url
