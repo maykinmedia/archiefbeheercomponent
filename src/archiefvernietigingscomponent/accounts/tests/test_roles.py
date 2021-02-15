33623a7bf6f4d085463bd91f48f3520d796879b1
silvia <silvia@maykinmedia.nl>
2021-02-15 15:45:04 +0100

    :white_check_mark: [#139] Added role type tests

---
+++ src/archiefvernietigingscomponent/accounts/tests/test_roles.py
@@ -0,0 +1,91 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+from archiefvernietigingscomponent.accounts.tests.factories import RoleFactory
+from archiefvernietigingscomponent.constants import RoleTypeChoices
+
+
+class RoleTests(TestCase):
+    def test_create_record_manager_with_correct_permissions(self):
+        role = RoleFactory.create(
+            type=RoleTypeChoices.record_manager,
+            can_start_destruction=True,
+            can_view_case_details=True,
+        )
+
+        role.clean()  # Shouldn't raise exceptions
+
+    def test_create_record_manager_with_insufficient_permissions(self):
+        role = RoleFactory.create(
+            type=RoleTypeChoices.record_manager,
+            can_start_destruction=False,
+            can_view_case_details=True,
+        )
+
+        with self.assertRaises(ValidationError):
+            role.clean()
+
+    def test_create_process_owner_with_correct_permissions(self):
+        role = RoleFactory.create(
+            type=RoleTypeChoices.process_owner,
+            can_review_destruction=True,
+            can_view_case_details=True,
+        )
+
+        role.clean()  # Shouldn't raise exceptions
+
+    def test_create_process_owner_with_insufficient_permissions(self):
+        role = RoleFactory.create(
+            type=RoleTypeChoices.process_owner,
+            can_review_destruction=True,
+            can_view_case_details=False,
+        )
+
+        with self.assertRaises(ValidationError):
+            role.clean()
+
+    def test_create_archivist_with_correct_permissions(self):
+        role = RoleFactory.create(
+            type=RoleTypeChoices.archivist, can_review_destruction=True,
+        )
+
+        role.clean()  # Shouldn't raise exceptions
+
+    def test_create_archivist_with_insufficient_permissions(self):
+        role = RoleFactory.create(
+            type=RoleTypeChoices.record_manager, can_review_destruction=False,
+        )
+
+        with self.assertRaises(ValidationError):
+            role.clean()
+
+    def test_create_functional_admin_with_correct_permissions(self):
+        role = RoleFactory.create(
+            type=RoleTypeChoices.functional_admin,
+            can_review_destruction=True,
+            can_start_destruction=True,
+            can_view_case_details=True,
+        )
+
+        role.clean()  # Shouldn't raise exceptions
+
+    def test_create_functional_admin_with_insufficient_permissions(self):
+        role = RoleFactory.create(
+            type=RoleTypeChoices.functional_admin,
+            can_review_destruction=True,
+            can_view_case_details=False,
+            can_start_destruction=True,
+        )
+
+        with self.assertRaises(ValidationError):
+            role.clean()
+
+    def test_create_other_role_without_permissions(self):
+        role = RoleFactory.create(
+            type=RoleTypeChoices.other,
+            can_review_destruction=False,
+            can_start_destruction=False,
+            can_view_case_details=False,
+        )
+
+        role.clean()  # Shouldn't raise exceptions
