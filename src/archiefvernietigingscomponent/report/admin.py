0ad0b1150a5274e3292ef3df461be6de8afe505e
silvia <silvia@maykinmedia.nl>
2021-02-24 15:12:13 +0100

    :lipstick: [#55] Read only fields

--- src/archiefvernietigingscomponent/report/admin.py
+++ src/archiefvernietigingscomponent/report/admin.py
@@ -8,12 +8,12 @@ from archiefvernietigingscomponent.report.views import DownloadDestructionReport
 
 @admin.register(DestructionReport)
 class DestructionReportAdmin(admin.ModelAdmin):
-    list_display = ("title", "process_owner")
-    list_filter = ("title", "process_owner")
-    search_fields = ("title", "process_owner")
+    list_display = ("title", "destruction_list", "process_owner")
+    list_filter = ("title", "destruction_list", "process_owner")
+    search_fields = ("title", "destruction_list", "process_owner")
 
     private_media_fields = ("content",)
     private_media_view_class = DownloadDestructionReportView
     private_media_file_widget = PrivateFileWidget
 
-    # TODO filter users to attach to the report
+    readonly_fields = ("title", "content", "destruction_list", "process_owner")
