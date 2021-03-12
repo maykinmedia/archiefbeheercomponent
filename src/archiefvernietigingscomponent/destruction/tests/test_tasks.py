1e6512d1b8676414a84c70541cdea18268510e91
silvia <silvia@maykinmedia.nl>
2021-03-12 11:51:06 +0100

    :white_check_mark: [#177] Added tests

--- src/archiefvernietigingscomponent/destruction/tests/test_tasks.py
+++ src/archiefvernietigingscomponent/destruction/tests/test_tasks.py
@@ -19,6 +19,8 @@ from archiefvernietigingscomponent.notifications.models import Notification
 
 from ...accounts.tests.factories import UserFactory
 from ...constants import RoleTypeChoices
+from ...emails.constants import EmailTypeChoices
+from ...emails.tests.factories import AutomaticEmailFactory
 from ...report.models import DestructionReport
 from ...tests.utils import mock_service_oas_get
 from ..constants import ListItemStatus, ListStatus, ReviewStatus
@@ -358,6 +360,12 @@ class NotifyTests(TestCase):
 
     @override_settings(DEFAULT_FROM_EMAIL="email@test.avc")
     def test_all_deleted_cases_are_in_destruction_report(self, m):
+        AutomaticEmailFactory.create(
+            type=EmailTypeChoices.report_available,
+            body="Report is available!",
+            subject="Report",
+        )
+
         archivaris = UserFactory.create(
             role__type=RoleTypeChoices.archivist,
             role__can_review_destruction=True,
@@ -433,14 +441,11 @@ class NotifyTests(TestCase):
 
         sent_mail = mail.outbox[0]
 
-        self.assertEqual(
-            "Declaration of destruction - Nice list (2021-02-15)", sent_mail.subject
-        )
+        self.assertEqual("Report", sent_mail.subject)
         self.assertEqual("email@test.avc", sent_mail.from_email)
         self.assertIn(archivaris.email, sent_mail.to)
         self.assertEqual(
-            "Destruction list 'Nice list' has been processed. The report of destruction is attached to this email.",
-            sent_mail.body,
+            "Report is available!", sent_mail.body,
         )
         self.assertEqual(1, len(sent_mail.attachments))
 
--- src/archiefvernietigingscomponent/emails/tests/test_models.py
+++ src/archiefvernietigingscomponent/emails/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.core import mail
 from django.test import TestCase
 
+from archiefvernietigingscomponent.accounts.tests.factories import UserFactory
 from archiefvernietigingscomponent.emails.tests.factories import AutomaticEmailFactory
 from archiefvernietigingscomponent.report.tests.factories import (
     DestructionReportFactory,
@@ -8,16 +9,21 @@ from archiefvernietigingscomponent.report.tests.factories import (
 
 
 class AutomaticEmailTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserFactory.create(email="test@example.com")
+
     def test_email_body_and_subject(self):
         email = AutomaticEmailFactory(
             body="This is a test text", subject="This is a test subject"
         )
-        email.send(recipient="test@example.com")
+        email.send(recipient=self.user)
 
         self.assertEqual(1, len(mail.outbox))
 
         sent_mail = mail.outbox[0]
 
+        self.assertEqual("test@example.com", sent_mail.to[0])
         self.assertEqual("This is a test text", sent_mail.body)
         self.assertEqual("This is a test subject", sent_mail.subject)
 
@@ -27,12 +33,13 @@ class AutomaticEmailTest(TestCase):
             body="This is a test text", subject="This is a test subject"
         )
 
-        email.send(recipient="test@example.com", report=report)
+        email.send(recipient=self.user, report=report)
 
         self.assertEqual(1, len(mail.outbox))
 
         sent_mail = mail.outbox[0]
 
+        self.assertEqual("test@example.com", sent_mail.to[0])
         self.assertEqual("This is a test text", sent_mail.body)
         self.assertEqual("This is a test subject", sent_mail.subject)
         self.assertEqual(1, len(sent_mail.attachments))
