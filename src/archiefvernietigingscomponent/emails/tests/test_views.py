078fcc06356fae9f2358f1842c6fac7855f61cf6
silvia <silvia@maykinmedia.nl>
2021-03-15 11:08:39 +0100

    :white_check_mark: [#178] Added email link test

--- src/archiefvernietigingscomponent/emails/tests/test_views.py
+++ src/archiefvernietigingscomponent/emails/tests/test_views.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from django_webtest import WebTest
+
 from archiefvernietigingscomponent.accounts.tests.factories import UserFactory
+from archiefvernietigingscomponent.constants import RoleTypeChoices
 
 
 class EmailPreferenceUpdateTest(TestCase):
@@ -25,3 +28,47 @@ class EmailPreferenceUpdateTest(TestCase):
         )
 
         self.assertEqual(200, response.status_code)
+
+
+class EmailPreferenceLinkTest(WebTest):
+    def test_link_process_owner(self):
+        process_owner = UserFactory.create(
+            role__can_review_destruction=True, role__type=RoleTypeChoices.process_owner
+        )
+
+        response = self.app.get(reverse("entry"), user=process_owner).follow()
+
+        self.assertEqual(200, response.status_code)
+
+        url_email_preferences = reverse(
+            "emails:email-preference-update", args=[process_owner.emailpreference.pk]
+        )
+
+        link_present = False
+        for link in response.html.find_all("a"):
+            if link.attrs["href"] == url_email_preferences:
+                link_present = True
+                break
+
+        self.assertTrue(link_present)
+
+    def test_link_record_manager(self):
+        record_manager = UserFactory.create(
+            role__can_start_destruction=True, role__type=RoleTypeChoices.record_manager
+        )
+
+        response = self.app.get(reverse("entry"), user=record_manager).follow()
+
+        self.assertEqual(200, response.status_code)
+
+        url_email_preferences = reverse(
+            "emails:email-preference-update", args=[record_manager.emailpreference.pk]
+        )
+
+        link_present = False
+        for link in response.html.find_all("a"):
+            if link.attrs["href"] == url_email_preferences:
+                link_present = True
+                break
+
+        self.assertTrue(link_present)
