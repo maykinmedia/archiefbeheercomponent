9c14800f48a8b94bfd83bc15fe982344a2527c33
unungentium@github.com <franek@maykinmedia.nl>
2021-11-12 12:24:17 +0100

    [#244] reformatting using black

--- src/archiefvernietigingscomponent/demo/management/commands/create-demo-data.py
+++ src/archiefvernietigingscomponent/demo/management/commands/create-demo-data.py
@@ -32,7 +32,8 @@ class Command(BaseCommand):
 
         # Create a catalogus if one doesn't exist already
         catalogus_list = ztc_client.list(
-            resource="catalogus", query_params={"domein": CATALOGUS['domein'], "rsin": CATALOGUS['rsin'],}
+            resource="catalogus",
+            query_params={"domein": CATALOGUS["domein"], "rsin": CATALOGUS["rsin"],},
         )
 
         if len(catalogus_list["results"]) == 0:
