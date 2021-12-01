721e81a5273c3416d0d2156f9585e0727cca2cff
silvia <silvia@maykinmedia.nl>
2021-12-01 14:34:23 +0100

    :white_check_mark: [#254] Test zaaktype choices in context

--- src/archiefvernietigingscomponent/destruction/tests/test_zaken_no_archive_date.py
+++ src/archiefvernietigingscomponent/destruction/tests/test_zaken_no_archive_date.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from django.test import TestCase
 from django.urls import reverse
 
@@ -13,10 +15,18 @@ class ZakenWithoutArchiveDateViewTests(TestCase):
 
         self.assertEqual(403, response.status_code)
 
-    def test_can_access_with_can_start_destruction(self):
+    @patch(
+        "archiefvernietigingscomponent.destruction.views.get_zaaktype_choices",
+        return_value=[{"url": "http://test.zaaktype.nl"}],
+    )
+    def test_can_access_with_can_start_destruction(self, m_choices):
         user = UserFactory(role__can_start_destruction=True)
         self.client.force_login(user)
 
         response = self.client.get(reverse("destruction:zaken-without-archive-date"))
 
         self.assertEqual(200, response.status_code)
+        self.assertIn("zaaktypen", response.context_data)
+        self.assertEqual(
+            "http://test.zaaktype.nl", response.context_data["zaaktypen"][0]["url"]
+        )
