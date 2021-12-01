da7f7f38f1a25a56168ee3bcbe3488a78c3a0e15
silvia <silvia@maykinmedia.nl>
2021-12-01 18:17:46 +0100

    :white_check_mark: [#240] Updated tests

--- src/archiefvernietigingscomponent/destruction/tests/test_update_zaak_archive_details.py
+++ src/archiefvernietigingscomponent/destruction/tests/test_update_zaak_archive_details.py
@@ -9,7 +9,7 @@ from zds_client.client import ClientError
 
 from archiefvernietigingscomponent.accounts.tests.factories import UserFactory
 
-from ..constants import ZaakArchiefnominatieChoices
+from ..constants import Archiefnominatie, Archiefstatus
 
 
 class UpdateZaakArchiveDetailsTests(WebTest):
@@ -21,32 +21,40 @@ class UpdateZaakArchiveDetailsTests(WebTest):
 
         self.assertEqual(403, response.status_code)
 
-    def test_can_access_with_can_start_destruction(self):
+    def test_permission_denied_if_no_zaak_url(self):
         user = UserFactory(role__can_start_destruction=True)
         self.client.force_login(user)
 
         response = self.client.get(reverse("destruction:update-zaak-archive-details"))
 
-        self.assertEqual(200, response.status_code)
+        self.assertEqual(403, response.status_code)
 
-    def test_wrong_query_params_displays_empty_form(self):
+    @patch("archiefvernietigingscomponent.destruction.views.fetch_zaak")
+    def test_can_access_with_can_start_destruction_and_zaak_url(self, m_fetch_zaak):
         user = UserFactory(role__can_start_destruction=True)
-        view_url = furl(reverse("destruction:update-zaak-archive-details"))
-        view_url.args["url"] = "an-invalid-url"
+        self.client.force_login(user)
 
-        response = self.app.get(view_url.url, user=user)
+        form_url = furl(reverse("destruction:update-zaak-archive-details"))
+        form_url.args.set("url", "http://openzaak.nl/some/zaak")
+
+        response = self.client.get(form_url.url)
 
         self.assertEqual(200, response.status_code)
 
-        form = response.form
-
-        self.assertEqual("", form["url"].value)
-
-    def test_fill_initial_data_with_query_params(self):
+    @patch(
+        "archiefvernietigingscomponent.destruction.views.fetch_zaak",
+        return_value={
+            "url": "http://openzaak.nl/some/valid/zaak/url",
+            "identificatie": "ZAAK-1",
+            "archiefnominatie": "blijvend_bewaren",
+            "archiefstatus": "nog_te_archiveren",
+            "archiefactiedatum": "2030-02-01",
+        },
+    )
+    def test_fill_initial_data(self, m_fetch_zaak):
         user = UserFactory(role__can_start_destruction=True)
         view_url = furl(reverse("destruction:update-zaak-archive-details"))
         view_url.args["url"] = "http://openzaak.nl/some/valid/zaak/url"
-        view_url.args["archiefnominatie"] = ZaakArchiefnominatieChoices.blijvend_bewaren
 
         response = self.app.get(view_url.url, user=user)
 
@@ -56,21 +64,28 @@ class UpdateZaakArchiveDetailsTests(WebTest):
 
         self.assertEqual("http://openzaak.nl/some/valid/zaak/url", form["url"].value)
         self.assertEqual(
-            ZaakArchiefnominatieChoices.blijvend_bewaren, form["archiefnominatie"].value
+            Archiefnominatie.blijvend_bewaren, form["archiefnominatie"].value
         )
+        self.assertEqual(Archiefstatus.nog_te_archiveren, form["archiefstatus"].value)
+        self.assertEqual("1", form["archiefactiedatum_day"].value)
+        self.assertEqual("2", form["archiefactiedatum_month"].value)
+        self.assertEqual("2030", form["archiefactiedatum_year"].value)
 
+    @patch("archiefvernietigingscomponent.destruction.views.fetch_zaak")
     @patch("archiefvernietigingscomponent.destruction.views.update_zaak",)
-    def test_submit_successful_form_redirects(self, m_update_zaak):
+    def test_submit_successful_form_redirects(self, m_update_zaak, m_fetch_zaak):
         user = UserFactory(role__can_start_destruction=True)
         view_url = furl(reverse("destruction:update-zaak-archive-details"))
+        view_url.args.set("url", "http://openzaak.nl/some/zaak")
 
         form = self.app.get(view_url.url, user=user).form
 
         form["url"] = "http://openzaak.nl/some/valid/zaak/url"
-        form["archiefnominatie"] = ZaakArchiefnominatieChoices.blijvend_bewaren
+        form["archiefnominatie"] = Archiefnominatie.blijvend_bewaren
         form["archiefactiedatum_day"] = "1"
         form["archiefactiedatum_month"] = "1"
-        form["archiefactiedatum_year"] = "2021"
+        form["archiefactiedatum_year"] = "2030"
+        form["archiefstatus"] = Archiefstatus.nog_te_archiveren
         form["comment"] = "Some interesting comment"
 
         response = form.submit()
@@ -81,28 +96,31 @@ class UpdateZaakArchiveDetailsTests(WebTest):
         m_update_zaak.assert_called_with(
             "http://openzaak.nl/some/valid/zaak/url",
             {
-                "archiefnominatie": ZaakArchiefnominatieChoices.blijvend_bewaren,
-                "archiefactiedatum": "2021-01-01",
+                "archiefnominatie": Archiefnominatie.blijvend_bewaren,
+                "archiefstatus": Archiefstatus.nog_te_archiveren,
+                "archiefactiedatum": "2030-01-01",
             },
             audit_comment="Some interesting comment",
         )
 
+    @patch("archiefvernietigingscomponent.destruction.views.fetch_zaak")
     @patch(
         "archiefvernietigingscomponent.destruction.views.update_zaak",
         side_effect=ClientError,
     )
     @override_settings(LANGUAGE_CODE="en")
-    def test_client_error_during_update(self, m_update_zaak):
+    def test_client_error_during_update(self, m_update_zaak, m_fetch_zaak):
         user = UserFactory(role__can_start_destruction=True)
         view_url = furl(reverse("destruction:update-zaak-archive-details"))
+        view_url.args.set("url", "http://openzaak.nl/some/zaak")
 
         form = self.app.get(view_url.url, user=user).form
 
         form["url"] = "http://openzaak.nl/some/valid/zaak/url"
-        form["archiefnominatie"] = ZaakArchiefnominatieChoices.blijvend_bewaren
+        form["archiefnominatie"] = Archiefnominatie.blijvend_bewaren
         form["archiefactiedatum_day"] = "1"
         form["archiefactiedatum_month"] = "1"
-        form["archiefactiedatum_year"] = "2021"
+        form["archiefactiedatum_year"] = "2030"
         form["comment"] = "Some interesting comment"
 
         response = form.submit()
@@ -112,15 +130,17 @@ class UpdateZaakArchiveDetailsTests(WebTest):
             response, "An error has occurred. The case could not be updated."
         )
 
+    @patch("archiefvernietigingscomponent.destruction.views.fetch_zaak")
     @patch("archiefvernietigingscomponent.destruction.views.update_zaak",)
-    def test_empty_archiefactiedatum(self, m_update_zaak):
+    def test_empty_archiefactiedatum(self, m_update_zaak, m_fetch_zaak):
         user = UserFactory(role__can_start_destruction=True)
         view_url = furl(reverse("destruction:update-zaak-archive-details"))
+        view_url.args.set("url", "http://openzaak.nl/some/zaak")
 
         form = self.app.get(view_url.url, user=user).form
 
         form["url"] = "http://openzaak.nl/some/valid/zaak/url"
-        form["archiefnominatie"] = ZaakArchiefnominatieChoices.blijvend_bewaren
+        form["archiefnominatie"] = Archiefnominatie.blijvend_bewaren
         form["comment"] = "Some interesting comment"
 
         response = form.submit()
@@ -130,6 +150,6 @@ class UpdateZaakArchiveDetailsTests(WebTest):
         )
         m_update_zaak.assert_called_with(
             "http://openzaak.nl/some/valid/zaak/url",
-            {"archiefnominatie": ZaakArchiefnominatieChoices.blijvend_bewaren},
+            {"archiefnominatie": Archiefnominatie.blijvend_bewaren},
             audit_comment="Some interesting comment",
         )
