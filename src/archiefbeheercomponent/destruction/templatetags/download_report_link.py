bac965740b41624658766e7416b16dbb4b0609e4
SilviaAmAm <silvia@maykinmedia.nl>
2022-03-24 17:22:12 +0100

    :sparkles: [#288] View to download zip of reviewers docs

--- src/archiefbeheercomponent/destruction/templatetags/download_report_link.py
+++ src/archiefbeheercomponent/destruction/templatetags/download_report_link.py
@@ -1,9 +1,11 @@
 from django import template
+from django.db.models import Q
 from django.urls import reverse
 
 from archiefbeheercomponent.constants import RoleTypeChoices
-from archiefbeheercomponent.destruction.models import DestructionList
 from archiefbeheercomponent.report.utils import get_absolute_url
+
+from ..models import DestructionList
 
 register = template.Library()
 
@@ -14,21 +16,38 @@ def download_report_link(context: dict, destruction_list: DestructionList) -> di
 
     report = destruction_list.destructionreport_set.first()
 
+    tag_context = {"can_download": False}
+
     if not report:
-        return {"can_download": False}
+        return tag_context
 
     if (
-        report.process_owner == request.user
-        or request.user.role.type == RoleTypeChoices.functional_admin
+        report.process_owner != request.user
+        and request.user.role.type != RoleTypeChoices.functional_admin
     ):
-        url = get_absolute_url(
-            reverse("report:download-report", args=[report.pk]),
-            request=context["view"].request,
-        )
-        return {
+        return tag_context
+
+    url = get_absolute_url(
+        reverse("report:download-report", args=[report.pk]),
+        request=context["view"].request,
+    )
+    tag_context.update(
+        {
             "can_download": True,
             "url_csv": f"{url}?type=csv",
             "url_pdf": f"{url}?type=pdf",
         }
+    )
 
-    return {"can_download": False}
+    reviews = destruction_list.reviews.filter(~Q(additional_document__exact=""))
+    if reviews.count():
+        reviewers_documents_url = get_absolute_url(
+            reverse(
+                "destruction:download-reviewer-documents", args=[destruction_list.pk]
+            ),
+            request=context["view"].request,
+        )
+
+        tag_context["additional_documents"] = reviewers_documents_url
+
+    return tag_context
--- src/archiefbeheercomponent/destruction/views/export.py
+++ src/archiefbeheercomponent/destruction/views/export.py
@@ -1,17 +1,31 @@
 import io
+import os
+import zipfile
 from typing import List
 from uuid import uuid4
+from wsgiref.util import FileWrapper
 
 from django.contrib.auth.mixins import UserPassesTestMixin
-from django.http import HttpResponse, HttpResponseBadRequest
+from django.db.models import Q
+from django.http import (
+    Http404,
+    HttpResponse,
+    HttpResponseBadRequest,
+    StreamingHttpResponse,
+)
+from django.shortcuts import get_object_or_404
 from django.utils.translation import gettext as _
 from django.views import View
+from django.views.generic import DetailView
 
 import xlsxwriter
 from zgw_consumers.concurrent import parallel
 
-from ...accounts.models import User
+from archiefbeheercomponent.accounts.models import User
+from archiefbeheercomponent.constants import RoleTypeChoices
+
 from ..forms import ZakenUrlsForm
+from ..models import ArchiveConfig, DestructionList
 from ..service import fetch_zaken
 from ..utils import format_zaak_record, get_additional_zaak_info
 
@@ -86,3 +100,56 @@ class ExportZakenWithoutArchiveDateView(UserPassesTestMixin, View):
         output.seek(0)
 
         return output.read()
+
+
+class DownloadAdditionalReviewersDocumentsView(UserPassesTestMixin, DetailView):
+    """
+    Download a file uploaded by a reviewer during review
+    """
+
+    model = DestructionList
+
+    def test_func(self):
+        config = ArchiveConfig.get_solo()
+        if not config.destruction_report_downloadable:
+            return False
+
+        user = self.request.user
+        if not user.is_authenticated:
+            return False
+
+        if user.is_superuser:
+            return True
+
+        destruction_list = self.get_object()
+        if (
+            destruction_list.assignees.filter(
+                assignee=user, assignee__role__type=RoleTypeChoices.process_owner
+            ).exists()
+            or user.role.type == RoleTypeChoices.functional_admin
+        ):
+            return True
+
+        return False
+
+    def get(self, request, *args, **kwargs):
+        destruction_list = get_object_or_404(DestructionList.objects, pk=kwargs["pk"])
+        reviews = destruction_list.reviews.filter(~Q(additional_document__exact=""))
+
+        if not reviews.count():
+            raise Http404
+
+        temp_file = io.BytesIO()
+        with zipfile.ZipFile(temp_file, "w") as file:
+            for review in reviews:
+                filepath = review.additional_document.path
+                file.write(filepath, arcname=os.path.basename(filepath))
+
+        temp_file.seek(0)
+
+        response = StreamingHttpResponse(
+            FileWrapper(temp_file), content_type="application/zip",
+        )
+
+        response["Content-Disposition"] = "attachment;filename=reviewersDocuments.zip"
+        return response
