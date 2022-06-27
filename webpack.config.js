4eebdb6e6bd34996dfb8045588a7a9e9569bda22
SilviaAmAm <silvia@maykinmedia.nl>
2022-06-27 16:00:42 +0200

    :fire: Replace gulp with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
+const webpack = require('webpack');
 var paths = require('./build/paths');
 var argv = require('yargs').argv;
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 var isProduction = process.env.NODE_ENV === 'production';
 if (argv.production) {
@@ -9,12 +10,15 @@ if (argv.production) {
 
 /**
  * Webpack configuration
- * Run using "webpack" or "gulp js"
+ * Run using "webpack" or "npm run build"
  */
 module.exports = {
     // Path to the js entry point (source).
     entry: {
-        main: __dirname + '/' + paths.jsEntry,
+        [`${paths.package.name}-css`]: `${__dirname}/${paths.scssEntry}`,
+        [`admin_overrides`]:  `${__dirname}/${paths.sourcesRoot}sass/admin/admin_overrides.scss`,
+        [`core-css`]:  `${__dirname}/${paths.sourcesRoot}sass/screen.scss`,
+        [`core-js`]:  `${__dirname}/${paths.sourcesRoot}js/index.js`,
     },
 
     // Path to the (transpiled) js
@@ -22,6 +26,16 @@ module.exports = {
         path: __dirname + '/' + paths.jsDir, // directory
         filename: '[name].js' // file
     },
+
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new webpack.DefinePlugin({
+            STATIC_URL: JSON.stringify(process.env.STATIC_URL ?? '/static/'),
+        }),
+        new webpack.ProvidePlugin({
+            _: 'lodash',
+        }),
+    ],
 
     // Use --production to optimize output.
     mode: isProduction ? 'production' : 'development',
@@ -33,6 +47,39 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: 'babel-loader',
                 test: /.js?$/
+            },
+            // .scss
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    // Writes css files.
+                    MiniCssExtractPlugin.loader,
+
+                    // Loads CSS files.
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false
+                        },
+                    },
+
+                    // Runs postcss configuration (postcss.config.js).
+                    {
+                        loader: 'postcss-loader'
+                    },
+
+                    // Compiles .scss to .css.
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sassOptions: {
+                                comments: false,
+                                style: 'compressed'
+                            },
+                            sourceMap: argv.sourcemap
+                        },
+                    },
+                ],
             }
         ]
     },
